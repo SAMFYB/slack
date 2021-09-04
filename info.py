433503a79d60ef03f639d229ba60646c5c42ff5f
Sam Yong <myong@andrew.cmu.edu>
2021-09-04 11:54:46 -0400

    Info CLI options

--- info.py
+++ info.py
@@ -21,3 +21,44 @@ def channel_info(workspace, channel):
     with open(path, 'r') as jsonf:
         return json.load(jsonf)['channel_info']
 
+
+
+if '__main__' == __name__:
+    import sys
+    from pprint import pprint
+
+    if len(sys.argv) > 1:
+
+        if sys.argv[1] == 'workspaces':
+            pprint(workspaces())
+            sys.exit(0)
+
+        elif sys.argv[1] == 'channels':
+            if len(sys.argv) < 3:
+                print('Which workspace?')
+                sys.exit(1)
+            try:
+                pprint(channels(sys.argv[2]))
+                sys.exit(0)
+            except Exception:
+                print('Is {} a correct workspace?'.format(sys.argv[2]))
+                sys.exit(1)
+
+        elif sys.argv[1] == 'channel-info':
+            if len(sys.argv) < 4:
+                print('Which workspace and which channel?')
+                sys.exit(1)
+            try:
+                pprint(channel_info(sys.argv[2], sys.argv[3]))
+                sys.exit(0)
+            except Exception:
+                print('Is {} -> {} a correct workspace channel?'.format(sys.argv[2], sys.argv[3]))
+                sys.exit(1)
+
+    else:
+        print('Options:')
+        print('workspaces - Find names of Slack workspaces in data')
+        print('channels <workspace> - Find names of channels in Slack `workspace`')
+        print('channel-info <workspace> <channel> - Get relevant channel info from `channel` in `workspace`')
+        sys.exit(0)
+
