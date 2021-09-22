1c7912c235db2bca7b018a64c9ddf39554b3a3c7
Sam Yong <myong@andrew.cmu.edu>
2021-09-21 22:57:51 -0400

    People workspace users and channels

--- people.py
+++ people.py
@@ -5,3 +5,15 @@ def get_user_channels(workspace, user):
     channels = data.channels(workspace)
     return [channel for channel in channels if user in data.channel_info(workspace, channel)['members']]
 
+def workspace_users(workspace):
+    """ All users in `workspace` and channels where they are member """
+    channels = data.channels(workspace)
+    users = {}
+    for channel in channels:
+        members = data.channel_info(workspace, channel)['members']
+        for member in members:
+            if member not in users:
+                users[member] = []
+            users[member].append(channel)
+    return users
+
